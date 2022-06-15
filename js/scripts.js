cd8d87edbb24a8c53f620e33d7f5f1723130ff77
toineto <augusto.vilhena4@gmail.com>
2022-06-15 18:13:10 -0300

    add algummas alterações

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+  constructor(previousOperationText, currentOperationText) {
+    this.previousOperationText = previousOperationText;
+    this.currentOperationText = currentOperationText;
+    this.currentOperation = "";
+  }
+
+  addDigit(digit) {
+    if(digit === "." & this.currentOperationText.innerText.includes(".")){
+      return
+    }
+    this.currentOperation = digit;
+    this.updateScreen();
+  }
+  updateScreen() {
+    this.currentOperationText.innerText += this.currentOperation;
+  }
+}
+
+
+const calc = new Calculator(previousOperationText, currentOperationText);
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const value = e.target.innerText;
+
+    if (+value >= 0 || value === ".") {
+      calc.addDigit(value);
+    } else {
+      console.log("op: " + value);
+    }
+  })
+})
